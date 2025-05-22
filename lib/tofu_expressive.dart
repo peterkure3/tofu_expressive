fad0186063c10bae50da434afd99235953ef9d4e
Peter Kure <peterkure256@gmail.com>
2025-05-22 23:37:52 +0300

    Update Imports to use tofu theme and theme controller

--- lib/tofu_expressive.dart
+++ lib/tofu_expressive.dart
@@ -1,7 +1,4 @@
 library tofu_expressive;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/tofu_theme.dart';
+export 'src/theme_controller.dart';
