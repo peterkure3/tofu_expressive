0d623b44ac408be9697a2e5dbaa733a279d459d2
Peter Kure <peterkure256@gmail.com>
2025-05-22 23:38:26 +0300

    Created Tofu theme

---
+++ lib/src/tofu_theme.dart
@@ -0,0 +1,34 @@
+import 'package:flex_color_scheme/flex_color_scheme.dart';
+import 'package:flutter/material.dart';
+
+class TofuTheme {
+  static ThemeData light([Color? seedColor]) {
+    return FlexThemeData.light(
+      scheme: FlexScheme.espresso,
+      useMaterial3: true,
+      useMaterial3ErrorColors: true,
+      subThemesData: const FlexSubThemesData(
+        blendOnLevel: 20,
+        blendOnColors: true,
+      ),
+      visualDensity: VisualDensity.standard,
+      fontFamily: 'Roboto',
+      colors: seedColor != null ? FlexColorScheme.light(seedColor: seedColor).toScheme : null,
+    );
+  }
+
+  static ThemeData dark([Color? seedColor]) {
+    return FlexThemeData.dark(
+      scheme: FlexScheme.espresso,
+      useMaterial3: true,
+      useMaterial3ErrorColors: true,
+      subThemesData: const FlexSubThemesData(
+        blendOnLevel: 30,
+        blendOnColors: true,
+      ),
+      visualDensity: VisualDensity.standard,
+      fontFamily: 'Roboto',
+      colors: seedColor != null ? FlexColorScheme.dark(seedColor: seedColor).toScheme : null,
+    );
+  }
+}
