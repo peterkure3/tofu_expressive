2850ee6039c4761afb0634ca456b6eb6c0c3081b
Peter Kure <peterkure256@gmail.com>
2025-05-30 21:56:11 +0300

    Update: Theme controller

--- lib/src/theme_controller.dart
+++ lib/src/theme_controller.dart
@@ -2,13 +2,24 @@ import 'package:flutter/material.dart';
 
 class ThemeController extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.system;
+  Color _seedColor = Colors.teal;
 
   ThemeMode get themeMode => _themeMode;
-
   bool get isDarkMode => _themeMode == ThemeMode.dark;
+  Color get seedColor => _seedColor;
 
   void toggleTheme(bool isDark) {
     _themeMode = isDark ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
+
+  void setThemeMode(ThemeMode mode) {
+    _themeMode = mode;
+    notifyListeners();
+  }
+
+  void setSeedColor(Color color) {
+    _seedColor = color;
     notifyListeners();
   }
 }
