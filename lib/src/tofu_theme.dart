2057e756f6561399d6ad7026fa58e943a8f4a91e
Peter Kure <peterkure256@gmail.com>
2025-06-28 18:30:46 +0300

    Refactor to reduce code duplication

--- lib/src/tofu_theme.dart
+++ lib/src/tofu_theme.dart
@@ -3,14 +3,18 @@ import 'package:flutter/material.dart';
 
 /// Tofu Expressive theme
 class TofuTheme {
-  /// Light theme with default seed color
-  static ThemeData light({
+  static ThemeData _createTheme({
+    required Brightness brightness,
     Color? seedColor,
     FlexScheme scheme = FlexScheme.material,
     FlexSubThemesData? subThemesData,
     String fontFamily = 'Roboto',
   }) {
-    return FlexThemeData.light(
+    final flexThemeData = brightness == Brightness.light
+        ? FlexThemeData.light
+        : FlexThemeData.dark;
+
+    return flexThemeData(
       scheme: scheme,
       useMaterial3: true,
       subThemesData: subThemesData ??
@@ -29,6 +33,22 @@ class TofuTheme {
     );
   }
 
+  /// Light theme with default seed color
+  static ThemeData light({
+    Color? seedColor,
+    FlexScheme scheme = FlexScheme.material,
+    FlexSubThemesData? subThemesData,
+    String fontFamily = 'Roboto',
+  }) {
+    return _createTheme(
+      brightness: Brightness.light,
+      seedColor: seedColor,
+      scheme: scheme,
+      subThemesData: subThemesData,
+      fontFamily: fontFamily,
+    );
+  }
+
   /// Dark theme with default seed color
   static ThemeData dark({
     Color? seedColor,
@@ -36,22 +56,12 @@ class TofuTheme {
     FlexSubThemesData? subThemesData,
     String fontFamily = 'Roboto',
   }) {
-    return FlexThemeData.dark(
+    return _createTheme(
+      brightness: Brightness.dark,
+      seedColor: seedColor,
       scheme: scheme,
-      useMaterial3: true,
-      subThemesData: subThemesData ??
-          const FlexSubThemesData(
-            blendOnLevel: 20,
-            blendOnColors: true,
-          ),
-      visualDensity: VisualDensity.standard,
+      subThemesData: subThemesData,
       fontFamily: fontFamily,
-      colors: seedColor != null
-          ? FlexSchemeColor.from(
-              primary: seedColor,
-              secondary: seedColor.withAlpha((255 * 0.8).round()),
-            )
-          : null,
     );
   }
 }
